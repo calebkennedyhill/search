934a541970415a3feda1a14025f63ddce4ba8a26
Jeremy Juybari <jeremy.juybari@maine.edu>
2025-12-08 13:40:07 -0500

    Started organizing w_a-star and going through the puzzle_util. want to touch base before doing more major changes.

--- w_a-star_variants.py
+++ w_a-star_variants.py
@@ -1,34 +1,12 @@
 import numpy as np
 import puzzle_utilities as util
-import animate_solution as anim
-
 from numpy import linalg as la
 import pandas as pd
 import datetime
-import random
-import sys
-
-SIDE = int(sys.argv[1])
-TIMEOUT = int(sys.argv[2])
-NUM_RUNS = int(sys.argv[3])
-
-HOME = (np.arange(SIDE**2)+1)%(SIDE**2)
-
-def random_state():
-    here = util.puzzle_state(HOME)
-    for i in range(100):
-        here = random.choice(util.moves(here))
-    here.parent = None
-    here.d = 0
-    return here
-
-
-
 
 def heur(s: util.puzzle_state, w1: float, w2: float):
-    flattened = s.config.copy()
-    flattened = flattened.reshape(SIDE**2,)
-
+    # flattened = s.config.copy()
+    # flattened = flattened.reshape(SIDE**2,)
     return w1*s.d + w2*(util.manhattan_total(s.config) + util.inversion_dist(s.config) )
     
 
@@ -39,12 +17,12 @@ def get_path(end: util.puzzle_state):
     while here.parent is not None:
         bwds = np.append(bwds, here.parent)
         here = here.parent
-    n = len(bwds)
+    # n = len(bwds)
     return np.flip(bwds)
 
 def find_sol_and_write_metrics(init_state: util.puzzle_state, path: str, upper_limit: int, w1: float, w2: float):
     qew = [init_state]
-    min_heur = heur(init_state, w1=w1, w2=w2)
+    # min_heur = heur(init_state, w1=w1, w2=w2)
     found_states = set()
     num_states_explored = 1
     sol_found = False
@@ -53,7 +31,7 @@ def find_sol_and_write_metrics(init_state: util.puzzle_state, path: str, upper_l
 
     print('searching...\t\t\t\tw1 =',w1,'\tw2 =',w2) #\t\tsearch algorithm: wA-star\t\tw1=',w1,'w2=',w2)
     start_time = datetime.datetime.now()
-    while sol_found is False:        
+    while not sol_found:
         here = qew[0]
         qew = np.delete(qew,0)
 
@@ -121,27 +99,42 @@ def find_sol_and_write_metrics(init_state: util.puzzle_state, path: str, upper_l
 
     return(get_path(end))
 
-# metrics_path = '/Users/calebhill/Documents/misc_coding/search/variants'
-metrics_path = './variants'
+
+if __name__ == '__main__':
+    # python w_a-star_variants.py --side 2 --timeout 1000 --num_runs 1 --dir "./variants"
+    #TODO: create debugg config so can easily step through
+    import argparse
+    parser = argparse.ArgumentParser(description='Weighted A* variants for puzzle solving')
+    parser.add_argument('--side', type=int, help='Side length of the puzzle')
+    parser.add_argument('--timeout', type=int, help='Timeout limit for number of states explored, good number to start 1000')
+    parser.add_argument('--num_runs', type=int, help='Number of runs to perform')
+    parser.add_argument('--dir', type=str, default='./variants', help='Directory path for output files')
+    # metrics_path = '/Users/calebhill/Documents/misc_coding/search/variants'
+    args = parser.parse_args()
+
+    SIDE = args.side
+    TIMEOUT = args.timeout
+    NUM_RUNS = args.num_runs
+    HOME = (np.arange(SIDE ** 2) + 1) % (SIDE ** 2)
+    metrics_path = args.dir
+
+    print('\n***********************************************************************************************************************')
+    print('Starting', NUM_RUNS, 'runs with puzzle size', SIDE)
+    for i in range(NUM_RUNS):
+        initial_state = util.init_random_state(home=HOME)
+        print('---------------- Beginning problem number:', i,'----------------\n')
+        fresh_copy = initial_state.copy()
+        find_sol_and_write_metrics(fresh_copy, path=metrics_path, w1=0.0, w2=1.0, upper_limit=TIMEOUT)       # pure heuristic
+        fresh_copy = initial_state.copy()
+        find_sol_and_write_metrics(fresh_copy, path=metrics_path, w1=1.0, w2=1.0, upper_limit=TIMEOUT)       # usual A*
+        fresh_copy = initial_state.copy()
+        find_sol_and_write_metrics(fresh_copy, path=metrics_path, w1=1.0, w2=2.0, upper_limit=TIMEOUT)       # weighted A*
+        fresh_copy = initial_state.copy()
+        find_sol_and_write_metrics(fresh_copy, path=metrics_path, w1=1.0, w2=1.25, upper_limit=TIMEOUT)       # weighted A*
+        fresh_copy = initial_state.copy()
+        find_sol_and_write_metrics(fresh_copy, path=metrics_path, w1=1.0, w2=1.5, upper_limit=TIMEOUT)       # weighted A*
 
 
-print('\n***********************************************************************************************************************')
-print('Starting', NUM_RUNS, 'runs with puzzle size', SIDE)
-for i in range(NUM_RUNS):
-    initial_state = random_state()
-    print('---------------- Beginning problem number:', i,'----------------\n')
-    fresh_copy = initial_state.copy()
-    find_sol_and_write_metrics(fresh_copy, path=metrics_path, w1=0.0, w2=1.0, upper_limit=TIMEOUT)       # pure heuristic
-    fresh_copy = initial_state.copy()
-    find_sol_and_write_metrics(fresh_copy, path=metrics_path, w1=1.0, w2=1.0, upper_limit=TIMEOUT)       # usual A*
-    fresh_copy = initial_state.copy()
-    find_sol_and_write_metrics(fresh_copy, path=metrics_path, w1=1.0, w2=2.0, upper_limit=TIMEOUT)       # weighted A*
-    fresh_copy = initial_state.copy()
-    find_sol_and_write_metrics(fresh_copy, path=metrics_path, w1=1.0, w2=1.25, upper_limit=TIMEOUT)       # weighted A*
-    fresh_copy = initial_state.copy()
-    find_sol_and_write_metrics(fresh_copy, path=metrics_path, w1=1.0, w2=1.5, upper_limit=TIMEOUT)       # weighted A*
-
-
-print('---------------- Experiment finished. ----------------\n\n')
+    print('---------------- Experiment finished. ----------------\n\n')
 
 
